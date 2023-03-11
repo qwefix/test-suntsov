648a22a1f2a98e27ba9a9c3707e0e0dbcef0e811
Hramchenko Alexey <70804795+qwefix@users.noreply.github.com>
2023-03-11 19:22:34 +0300

    feat: add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,34 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const minify = require("gulp-minify");
-const sourceMaps = require("gulp-sourcemaps");
 const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
 
-const dist = "dist"
+const dist = "dist";
 
 gulp.task("html", () => {
-	return gulp
-	.src("src/index.html")
-	.pipe(gulp.dest(dist));
+	return gulp.src("src/index.html").pipe(gulp.dest(dist));
 });
 gulp.task("assets", () => {
-	return gulp
-	.src("src/assets/**")
-	.pipe(gulp.dest(dist+'/assets'));
+	return gulp.src("src/assets/**").pipe(gulp.dest(dist + "/assets"));
 });
 
 gulp.task("sass", (cb) => {
 	gulp
 		.src("src/styles/*.scss")
-		.pipe(sass().on("error", sass.logError))
+		.pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+		.pipe(
+			autoprefixer(
+				"last 2 versions",
+				"safari 5",
+				"ie 8",
+				"ie 9",
+				"Firefox 14",
+				"opera 12.1",
+				"ios 6",
+				"android 4"
+			)
+		)
 		.pipe(concat("style.css"))
 		.pipe(gulp.dest(dist));
 	cb();
@@ -36,7 +44,7 @@ gulp.task("js", (cb) => {
 	cb();
 });
 
-gulp.task("build", gulp.series("sass", "html", "js","assets"));
+gulp.task("build", gulp.series("sass", "html", "js", "assets"));
 
 gulp.task("dev", () => {
 	gulp.watch("src/styles/*.scss", gulp.series("sass"));
